22582add7bb082099e09993baa4598b1ea90657a
Michelle <michedomingo@gmail.com>
2020-05-20 05:04:18 +0000

    Update documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+This module creates a class Square.
+"""
+
 class Square:
     """
-    This is an empty class Square that defines a square.
+    This is an empty class Square that defines a square
     """
     pass
