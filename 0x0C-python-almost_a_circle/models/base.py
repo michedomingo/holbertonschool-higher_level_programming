3b611f04c6f41539f5f6dfddcaac98b980031267
michedomingo <michedomingo@gmail.com>
2020-06-09 01:44:08 -0700

    Base class will be the “base” of all other classes in this project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines class Base
+"""
+
+
+class Base:
+	"""
+	Defines the “base” of all other classes in this project
+	to manage id attribute/avoid code duplicating
+	*private class attribute: __nb_objects
+	*public instance attribute: id
+	"""
+	__nb_objects = 0
+
+	def __init__(self, id=None):
+		"""Initialize class Base instance by id"""
+		if id is not None:
+			self.id = id
+		else:
+			Base.__nb_objects += 1
+			self.id = Base.__nb_objects
