6a45751999dbd1f0c729779b8857b37352d3e180
michedomingo <michedomingo@gmail.com>
2020-07-14 14:59:16 -0700

    Task6 solution

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Creates the database hbtn_0d_usa and the table states with id and name
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL);
