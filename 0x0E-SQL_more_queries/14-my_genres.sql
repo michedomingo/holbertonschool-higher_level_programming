8196e33c2458aea7e96724d4e3c23097e87f28b5
michedomingo <michedomingo@gmail.com>
2020-07-15 02:53:52 +0000

    Update syntax

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,6 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 SELECT tv_genres.name FROM tv_show_genres
-       JOIN tv_shows ON tv_show_genres.show_id = tv_show.id
+       JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
        JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
        WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name;
