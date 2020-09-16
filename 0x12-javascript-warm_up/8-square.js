f1a4495ac5d0273d9fd7f25372a5c0f81a3ff0a5
michedomingo <michedomingo@gmail.com>
2020-09-16 01:29:29 +0000

    Task8 solution

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const input = process.argv[2];
+let i = 0;
+const size = parseInt(input);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
