5d5100221a53a5334e3d421c8f302a9d4f9af1bc
michedomingo <michedomingo@gmail.com>
2020-09-16 01:29:13 +0000

    Task7 solution

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const count = parseInt(process.argv[2], 10);
+let i;
+
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < count; i++) { console.log('C is fun'); }
+}
