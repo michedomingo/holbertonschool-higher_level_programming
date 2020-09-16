f5ce578277400494c88a81b056dbe422f7459309
michedomingo <michedomingo@gmail.com>
2020-09-16 01:30:17 +0000

    Task10 solution

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+function factorial (arg) {
+  if (isNaN(arg)) {
+    return (1);
+  } else if (arg === 0) {
+    return (1);
+  } else {
+    return (arg * factorial(arg - 1));
+  }
+}
+let input = process.argv[2];
+input = parseInt(input);
+console.log(factorial(input));
