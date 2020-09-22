6065e508dfe9558e1d93a007ae00e3af502bbcb3
michedomingo <michedomingo@gmail.com>
2020-09-22 03:35:42 +0000

    Task3 solution

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,16 +2,15 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (parseInt(h) > 0 && parseInt(w) > 0) {
-      this.height = h;
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
       this.width = w;
+      this.height = h;
     }
   }
 }
 
 Rectangle.prototype.print = function () {
-  let i = 0;
-  for (i; i < this.height; i++) {
+  for (let i = 0; i < this.height; i++) {
     console.log('X'.repeat(this.width));
   }
 };
