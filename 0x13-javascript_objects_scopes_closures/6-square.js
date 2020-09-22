61eb3c97f96f13ff1f2831e669dda197539ad8f3
michedomingo <michedomingo@gmail.com>
2020-09-22 03:21:36 +0000

    Task6 solution

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const s = require('./5-square');
+
+class Square extends s {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+
+module.exports = Square;
