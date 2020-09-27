e57e13a0a5e5ca921124c0d752bea21642922c91
michedomingo <michedomingo@gmail.com>
2020-09-26 23:31:17 -0700

    Task8 solution

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,4 @@
-#!/usr/bin/node
+$.getJSON('https://swapi.co/api/films/?format=json', function (data) {
+  data.results.forEach(function (film) {
+    $('#list_movies').append('<li>' + film.title + '</li>');
+  });
